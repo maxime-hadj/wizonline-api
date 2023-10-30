ed0a1fc9194cb14317f850df93cb06643c8d723c
max <maxime.hadj@laplateforme.io>
2023-10-30 10:50:12 +0100

    added connection test

--- server/connections/wizonline.js
+++ server/connections/wizonline.js
@@ -1,17 +1,24 @@
 const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
+const filepath = ('./db.sqlite')
 
+// db connection test
+// let db = new sqlite3.Database('./db.sqlite', err => {
+//   if (err)
+//   throw err;
+//   console.log('logged to db');
+// });
 
 function createDbConnection() {
   if (fs.existsSync(filepath)) {
-    return new sqlite3.Database(filepath);
+    return new sqlite3.Database(filepath, sqlite3.OPEN_READONLY);
     } else {
     const db = new sqlite3.Database(filepath, (error) => {
       if (error) {
         return console.error(error.message);
       }
     });
-    console.log('Connection with SQLite has been established.');
+    console.log('Successfully connected to the database.');
     return db;
   }
 }
